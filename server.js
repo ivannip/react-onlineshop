fd22a4eeba6b567949d6c42ee8d606eeb4077de8
Ivan <ihpn1802@gmail.com>
2023-01-23 17:41:19 +0000

    update for render deployment

--- server.js
+++ server.js
@@ -46,13 +46,13 @@ app.use("/order", orderRoute);
 //app.use("/msg", mqRouter);
 
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+//if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
   });
-}
+//}
 
 
 app.listen(PORT, () => {
